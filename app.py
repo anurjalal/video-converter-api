689f678702620aa7796ffe79d30ddd0ad4c97c8a
anurjalal <anurjalal@protonmail.com>
2020-08-22 05:19:33 +0700

    add upload validation and api response message

--- app.py
+++ app.py
@@ -6,9 +6,10 @@ import uuid
 import datetime
 import subprocess as sp
 import urllib
+import re
 from config import UPLOAD_FOLDER, RESULT_FOLDER, REDIS_URL, SECRET_KEY
 from time import sleep
-from flask import Flask, request, abort, jsonify, session
+from flask import Flask, request, abort, jsonify, session, send_from_directory, Response
 from collections import Counter
 from celery.utils.log import get_task_logger
 from celery.result import AsyncResult
@@ -33,32 +34,66 @@ class Video():
 
 @app.before_first_request
 def before_first_request_func():
-    session["filename"] = list()
+    session["upload_filename"] = list()
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    """Upload file endpoint."""
+    if request.method == 'POST':
+        if not request.files.get('file', None):
+            err_mssg = "A file is required!"
+            return abort(Response(err_mssg, 400))
+        received_file = request.files['file']
+        filename, ext = os.path.splitext(received_file.filename)
+        # check video format
+        if(not is_vid_format_valid(ext)):
+            err_mssg = f'video format of {filename} is not accepted.'
+            return abort(Response(err_mssg, 400))
+        dirname_by_day = str(datetime.date.today())
+        if(os.path.isdir(os.path.abspath(os.path.join(os.getcwd(), UPLOAD_FOLDER, dirname_by_day))) is not True):
+            try:
+                os.mkdir(os.path.abspath(os.path.join(os.getcwd(), UPLOAD_FOLDER, dirname_by_day)))
+            except OSError as exc:
+                logger.info(exc.args)
+        s_filename = secure_filename(received_file.filename)
+        s_filename, ext = os.path.splitext(s_filename)
+        unique_filename = f'{dirname_by_day}_{uuid.uuid4()}_{s_filename}{ext}'
+        saving_path = os.path.abspath(os.path.join(os.getcwd(), UPLOAD_FOLDER, dirname_by_day, unique_filename))
+        unique_path = os.path.join(dirname_by_day, unique_filename)
+        try:
+            received_file.save(saving_path)
+            temp = session.get("upload_filename")
+            temp.append(unique_path)
+            session["upload_filename"] = temp
+            return jsonify({
+            'succes': True,
+            'message': f'the file {received_file.filename} has been successfully uploaded'
+        })
+        except Exception as exc:
+            logger.info(exc.args)
+        err_mssg = f'the file {received_file.filename} failed to upload'
+        abort(Response(err_mssg, 400))
 
 @celery.task()
 def video_converter(unique_path, ext, crf, fps):
     filename, _ = os.path.splitext(unique_path)
-    logger.info(f'{filename}')
     print(str(unique_path))
     filename = filename.split('/')[1]
     s_filename = secure_filename(filename)
-    logger.info(f'{s_filename}')
     in_path = os.path.abspath(os.path.join(os.getcwd(), UPLOAD_FOLDER, unique_path))
     unique_filename = f'{s_filename}{ext}'
-    #command = f'ffmpeg -i {in_path} -crf={crf} -filter:v fps={fps} {unique_filename}'
     out_path = os.path.abspath(os.path.join(os.getcwd(), RESULT_FOLDER, unique_filename))
-    command = ['ffmpeg', '-y' ,'-i', str(in_path), '-crf', str(crf), '-filter:v' ,f'fps=fps={fps}' , str(out_path)] 
-    process = sp.Popen(command)
-    process.communicate()
+    command = ['ffmpeg', '-y' ,'-i', str(in_path), '-crf', str(crf), '-filter:v' ,f'fps=fps={fps}' , str(out_path)]
+    try:
+        process = sp.Popen(command, stdout=sp.PIPE, stderr=sp.PIPE)
+        _, err = process.communicate()
+    except Exception as ex:
+        logging.info(ex.args)
     return out_path
 
-@celery.task(name='file-saving-task')
-def save_file(file):
-    path = os.path.abspath(os.path.join(os.getcwd, UPLOAD_FOLDER, filename))
-
-@app.route('/video_converter', methods=['POST'])
+@app.route('/video_converter', methods=['GET'])
 def get_convert():
-    if request.method == 'POST':
+    if request.method == 'GET':
         unique_path = request.args.get('unique_path')
         ext = request.args.get('ext')
         crf = request.args.get('crf')
@@ -66,46 +101,15 @@ def get_convert():
         print(unique_path, ext, crf, fps)
         task = video_converter.delay(unique_path, ext, crf, fps)
         async_result = AsyncResult(id=task.task_id, app=celery)
-        url = async_result.get()
-        return url
+        out_path = async_result.get()
+        return send_from_directory(os.path.dirname(out_path), os.path.basename(out_path))
 
-@app.route('/get', methods=['POST'])
-def get_cache():
-    if request.method == 'POST':
-        a = session.get("filename")
-        return jsonify(cachess = a)
-
-@app.route('/upload', methods=['POST'])
-def upload():
-    """Upload file endpoint."""
-    if request.method == 'POST':
-        if not request.files.get('file', None):
-            msg = 'the request contains no file'
-            logger.error(msg)
-
-        received_file = request.files['file']
-        if received_file:  #tambahkan validasi
-            msg = f'the file {received_file.filename} has wrong extention'
-            logger.error(msg)
-        dirname_by_day = str(datetime.date.today())
-        if(os.path.isdir(os.path.abspath(os.path.join(os.getcwd(), UPLOAD_FOLDER, dirname_by_day))) is not True):
-            try:
-                os.mkdir(os.path.abspath(os.path.join(os.getcwd(), UPLOAD_FOLDER, dirname_by_day)))
-            except OSError as exc:
-                print(exc.args)
-        s_filename = secure_filename(received_file.filename)
-        s_filename, file_extension = os.path.splitext(s_filename)
-        unique_filename = f'{dirname_by_day}_{uuid.uuid4()}_{s_filename}{file_extension}'
-        saving_path = os.path.abspath(os.path.join(os.getcwd(), UPLOAD_FOLDER, dirname_by_day, unique_filename))
-        unique_path = os.path.join(dirname_by_day, unique_filename)
-        try:
-            received_file.save(saving_path)
-            temp = session.get("filename")
-            temp.append(unique_path)
-            session["filename"] = temp
-            print(str(session["filename"]))
-        except Exception as exc:
-            print(exc.args)
-            pass
-        logger.info(f'the file {received_file.filename} has been successfully saved as {unique_filename}')
-        return s_filename
+def is_vid_format_valid(ext):
+    ext_without_dot = ext.replace('.', '')
+    cmd = ['ffmpeg', '-h' ,f'muxer={ext_without_dot}']
+    process = sp.run(cmd, stdout=sp.PIPE)
+    inv_messg = re.search("Unknown format+", str(process.stdout))
+    if inv_messg:
+        return False
+    else:
+        return True
